da033e2848c35c73a58f7fa94f30a725bd5a71f6
patterson <1479009707@qq.com>
2020-06-12 15:36:26 +0800

    修改线上接口请求地址

--- config/index.js
+++ config/index.js
@@ -16,7 +16,7 @@ module.exports = {
         // target: 'http://case.net/api/',
         changeOrigin:true,//允许跨域
         pathRewrite:{
-          '/api':''
+          '/api':'http://office365.aladdinlaw.com:3921/api/'
         }
       }
     },
