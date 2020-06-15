80646a1b76a8839dd31bf609e5aa4e5b2d73db71
patterson <1479009707@qq.com>
2020-06-15 18:07:40 +0800

    修改域名地址

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api':{
-        target:'http://office365.aladdinlaw.com:3921/api/',
+        target:'http://jiawen.jialilawyer.com/pug/api/',
         // target: 'http://case.net/api/',
         changeOrigin:true,//允许跨域
         pathRewrite:{
