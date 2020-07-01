d79a57541e7a3ba74f950bd0986687c165582bca
patterson <1479009707@qq.com>
2020-07-01 13:44:59 +0800

    离婚知识和离婚帮助内容页左侧导航悬浮加粗

--- src/components/LiHun/Ad/KnowledgeCon.vue
+++ src/components/LiHun/Ad/KnowledgeCon.vue
@@ -6,7 +6,7 @@
         <div class="pt-10">
           <h3 class="text-center text-lg font-bold">离婚知识</h3>
           <ul class="mt-5">
-            <li v-for="(item, index) in fenleiAll" :key="index" class="text-base leading-loose text-center">
+            <li v-for="(item, index) in fenleiAll" :key="index" class="text-base leading-loose hover:font-bold text-center">
               <h2 @click="searchList(item, index)">{{ item.title }}</h2>
               <!-- <ul class="pl-2" v-if="item.data.length>0">
                 <li v-for="($item, $index) in item.data" :key="$index"  @click="searchList($item)">{{ $item.title }}</li>
