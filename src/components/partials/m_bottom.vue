27aa6ff19367c07ba87d5b080223fd0a169a93ac
patterson <1479009707@qq.com>
2020-08-28 11:31:33 +0800

    替换电话

--- src/components/partials/m_bottom.vue
+++ src/components/partials/m_bottom.vue
@@ -4,7 +4,7 @@
 			<div class="m_bottom_all text-left px-3 mt-2">
 				<h2 class="text-lg text-white py-6 text-bold">联系我们</h2>
 				<div class="border-b border-gray-500 pb-3">
-					<p class="text-xs text-white">电话：400-8989-818     时间：周一至周五 09:00-18:00</p>
+					<p class="text-xs text-white">电话：178-8882-8870     时间：周一至周五 09:00-18:00</p>
 					<p class="text-xs text-white">地址：北京市建国门外大街22号赛特大厦3层</p>
 				</div>
 				<div class="pt-3 pb-10">
